042ec689fe335c2523b6a995c3898969598d854c
Mike Bostock <michael.bostock@opencastsoftware.com>
2023-03-24 15:23:47 +0000

    📝 refactoring implementation >5

--- app/src/main/kotlin/RomanNumerals.kt
+++ app/src/main/kotlin/RomanNumerals.kt
@@ -3,7 +3,7 @@ object RomanNumerals {
         return when {
             value == 10 -> "X"
             value == 9 -> "IX"
-            value > 5 -> "V" + fromDecimal(value % 5)
+            value > 5 -> "V" + fromDecimal(value - 5)
             value == 5 -> "V"
             value == 4 -> "IV"
             else -> "I".repeat(value)
