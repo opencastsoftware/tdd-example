ed1e53623cb941a4859ef1b6bf33b4dc8878d050
Mike Bostock <michael.bostock@opencastsoftware.com>
2023-03-24 15:22:18 +0000

    🟢 pass test for 10 returning X

--- app/src/main/kotlin/RomanNumerals.kt
+++ app/src/main/kotlin/RomanNumerals.kt
@@ -1,6 +1,7 @@
 object RomanNumerals {
     fun fromDecimal(value: Int): String {
         return when {
+            value == 10 -> "X"
             value == 9 -> "IX"
             value > 5 -> "V" + fromDecimal(value % 5)
             value == 5 -> "V"
