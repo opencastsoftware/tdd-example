956b9690842105a4723f3a0fc9d2474d5749264e
Mike Bostock <michael.bostock@opencastsoftware.com>
2023-03-24 14:33:51 +0000

    🟢 pass test for 5 returning V for specific case

--- app/src/main/kotlin/RomanNumerals.kt
+++ app/src/main/kotlin/RomanNumerals.kt
@@ -1,6 +1,7 @@
 object RomanNumerals {
     fun fromDecimal(value: Int): String {
         return when (value) {
+            5 -> "V"
             4 -> "IV"
             else -> "I".repeat(value)
         }
