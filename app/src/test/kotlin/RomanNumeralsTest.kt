a362608a1986d776480201f8ee4e9c281ff5fca5
Mike Bostock <michael.bostock@opencastsoftware.com>
2023-04-26 11:53:33 +0100

    🔴 failing test for 9 returning IX

--- app/src/test/kotlin/RomanNumeralsTest.kt
+++ app/src/test/kotlin/RomanNumeralsTest.kt
@@ -14,4 +14,8 @@ internal class RomanNumeralsTest {
     internal fun `should return V for 5`() {
         assertEquals("V", RomanNumerals.fromDecimal(5))
     }
+    @Test
+    internal fun `should return IX for 9`() {
+        assertEquals("IX", RomanNumerals.fromDecimal(9))
+    }
 }
