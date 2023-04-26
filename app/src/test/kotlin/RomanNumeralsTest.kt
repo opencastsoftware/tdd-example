41f707e75b61a9dedfb7c44391f5586d03e1fd3e
Mike Bostock <michael.bostock@opencastsoftware.com>
2023-04-26 11:37:27 +0100

    🔴 failing test for 4 returning IV

--- app/src/test/kotlin/RomanNumeralsTest.kt
+++ app/src/test/kotlin/RomanNumeralsTest.kt
@@ -6,4 +6,8 @@ internal class RomanNumeralsTest {
     internal fun `should return I for 1`() {
         assertEquals("I", RomanNumerals.fromDecimal(1))
     }
+    @Test
+    internal fun `should return IV for 4`() {
+        assertEquals("IV", RomanNumerals.fromDecimal(4))
+    }
 }
